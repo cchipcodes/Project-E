cd5f9f43de1dce3845b039fbfaf4977380b7faf5
nathan. <101525891+cchipcodes@users.noreply.github.com>
2026-07-07 19:51:52 +0100

    Testing spawning logic

--- Idea5/src/game/abilities.ts
+++ Idea5/src/game/abilities.ts
@@ -2,5 +2,11 @@ import * as J from "jamango";
 import { PlayerTrait, BlankCardTrait } from "../traits";
 
 export function cardLogic() {
-    const blankCard = J.getPropAsset(J.getAllWithTraits([BlankCardTrait]).findIndex[0])
+    const blankCard = J.getPropAsset(J.getAllWithTraits([BlankCardTrait]).findIndex[0]);
+    const plr = J.getLocalPlayer();
+    const plrTrait = J.getTrait(plr, PlayerTrait);
+    J.onGameRender(() => {
+        const card = J.spawnProp(blankCard);
+        J.setEntityPosition(card, J.getEntityPosition(plr))
+    })
 };
