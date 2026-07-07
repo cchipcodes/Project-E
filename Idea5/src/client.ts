31964dfa46e8491c235507394c574c6c99e73998
nathan. <101525891+cchipcodes@users.noreply.github.com>
2026-07-07 19:53:15 +0100

    Imported cardLogic function to client script to test in game

--- Idea5/src/client.ts
+++ Idea5/src/client.ts
@@ -3,9 +3,11 @@ import "./traits";
 import { initClientSystems } from "./client/systems";
 import { initVehicleUI } from "./client/vehicle-ui";
 import { initVehicleSystem } from "./shared/vehicle";
+import { cardLogic } from "./game/abilities";
 
 if (J.net.isClient) {
     initClientSystems();
     initVehicleSystem();
     initVehicleUI();
+    cardLogic();
 }
