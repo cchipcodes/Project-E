a4742067d44911e4e5393c2b1182d1f9e0478f50
nathan. <101525891+cchipcodes@users.noreply.github.com>
2026-07-09 15:02:16 +0100

    Removed additional functions

--- Idea5/src/additionalFunctions.ts
+++
@@ -1,9 +0,0 @@
-import * as J from "jamango";
-
-export function wait(t: number) {
-    let lastRecordedTime = 0
-    const time = J.getWorldTime();
-    if (time - lastRecordedTime > t) {
-        lastRecordedTime = time;
-    };
-};
--- Idea5/src/game.ts
+++ Idea5/src/game.ts
@@ -1,10 +1,8 @@
 import * as J from "jamango";
-import * as game from "./additionalFunctions";
 
 //Server Functions
 export function spawnEnemy() {
-    game.wait(5)
-    J.sendChatMessage("This event took place after 5 seconds", "#FFFF");
+    
 };
 
 //Client Functions
