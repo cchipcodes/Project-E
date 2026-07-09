6b4e55c42fec12eec184b145503ef210e4177c19
nathan. <101525891+cchipcodes@users.noreply.github.com>
2026-07-09 15:48:44 +0100

    Removed redundant code from spawnEnemy

--- Idea5/src/game.ts
+++ Idea5/src/game.ts
@@ -7,9 +7,6 @@ export function spawnEnemy(interval: number) {
 
     J.onGameTick((_, time) => {
         if (time - lastSpawnTime > interval) {
-            const enemyPawn = J.getSceneTreeNodeEntity("Pawn");
-            if (!enemyPawn) return;
-            const blueprint = J.getTrait(enemyPawn, traits.ZombieTrait);
             const newEnemyPawn = J.spawnCharacter(J.assets.avatars.Pawn.id);
             J.setTrait(newEnemyPawn, traits.ZombieTrait)
             lastSpawnTime = time;
