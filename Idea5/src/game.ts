04adbb89e3e275c571615ba7bbeb7b0c3c589d5a
nathan. <101525891+cchipcodes@users.noreply.github.com>
2026-07-15 16:44:42 +0100

    Updated Blank Card Logic

--- Idea5/src/game.ts
+++ Idea5/src/game.ts
@@ -1,5 +1,6 @@
 import * as J from "jamango";
 import * as traits from "./traits/index"
+// Constants and Variables
 
 //Server Functions
 export function spawnEnemy(interval: number) {
@@ -37,6 +38,7 @@ export function useCard(type: string, duration: number, cooldown: number) {
             case "blank":
                 let lastAtkTime = 0;
                 let equipTime = 0;
+                let spinningBC: number | undefined;
                 J.onGameTick((_, time) => {
                     if (!J.getTrait(plr, traits.HeldItemTrait) && time - lastAtkTime > cooldown) {
                         const card = J.assets.props["Blank Card"]
@@ -44,23 +46,40 @@ export function useCard(type: string, duration: number, cooldown: number) {
                             enabled: true,
                             firstPerson: true,
                             source: {type: "prop", prop: card.id},
-                            slot: "waist",
-                            holdPose: "Tool",
+                            slot: "handRight",
+                            holdPose: "",
                             position: [0,0,0],
                             fpPosition: [0.5,-0.7,-0.7],
                             rotation: [0,0,0],
                             fpRotation: [0,0,0],
-                            scale: 0.25,
-                            fpScale: 0.25
+                            scale: 0.1,
+                            fpScale: 0.1
                         });
+                        spinningBC = J.spawnProp(card.id);
+                        J.setTrait(spinningBC, traits.SpinningTrait, {
+                            enabled: true,
+                            axis: [0, 1, 0],
+                            speedDegreesPerSecond: 90
+                        });
+                        J.setTrait(spinningBC, traits.EnemyDamageTrait, { damage: 10 });
+                        J.moveKinematicEntity(spinningBC, J.getEntityPosition(plr), [0,0,0,0]);
                         console.log("Equipped");
                         equipTime = time;
                     };
                     if (equipTime && time - equipTime > duration) {
                         J.removeTrait(plr,traits.HeldItemTrait);
+                        J.removeEntity(spinningBC);
                         console.log("Unequipped");
                         equipTime = 0;
                         lastAtkTime = time;
+                    };
+                    if (spinningBC) {
+                        const playerPos = J.getEntityPosition(plr);
+                        J.moveKinematicEntity(spinningBC, [
+                            playerPos[0],
+                            playerPos[1],
+                            playerPos[2],
+                        ], [0,0,0,0]);
                     };
                 })
             }
